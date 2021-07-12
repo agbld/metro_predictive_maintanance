9190a0d990d1e615ed0cc37817eed4385ed61901
agbld <agiblida@gmail.com>
2021-07-13 02:48:13 +0800

    data collect algo optimize, add comments,

--- get_dataset.py
+++ get_dataset.py
@@ -3,22 +3,36 @@ from os import path
 import datetime
 from datetime import date, timedelta
 import csv
+from pandas.core.frame import DataFrame
 import pymssql
 from scipy.sparse import data
+from tensorflow.python.framework.ops import device
 
 event_key_table_path = 'refer/事件警報對照表_toAFC.csv'
 collected_data_folder = 'collected_data/'
-mssql_host='wfx1.duckdns.org'
-mssql_user='agiblida'
-mssql_password='lc@32117'
-mssql_database='metro_maintenance'
+mssql_host=''
+mssql_user=''
+mssql_password=''
+mssql_database=''
+
+with open('refer/mssql_info.csv', newline='') as csvfile:
+  rows = csv.reader(csvfile)
+  mssql_host=next(rows)[0]
+  mssql_user=next(rows)[0]
+  mssql_password=next(rows)[0]
+  mssql_database=next(rows)[0]
+  
 
 #%%
-def get_device_keys_table(host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database, svce_loc_id_list=[]):
+# collect all device_key(s) from server, and return a distincted device_keys_table.
+# (optional) given a not empty [svce_loc_id_list] to collect devices in those stations list in [svce_loc_id_list]
+# (optional) set [use_archive]=False to always collect data from server insteed of csv
+# device_key : [svce_loc_id] + [dev_name]
+def get_device_keys_table(host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database, svce_loc_id_list=[], use_archive=True):
   print('\ncollecting device key table ...')
   key_table = [] # the device key table
   save_path = collected_data_folder + 'device_keys_table.csv'
-  if path.exists(save_path):
+  if path.exists(save_path) and use_archive:
     with open(save_path, newline='') as csvfile:
       rows = csv.reader(csvfile)
       for row in rows:
@@ -73,14 +87,18 @@ def get_device_keys_table(host=mssql_host, user=mssql_user, passward=mssql_passw
   print('device key table archived.')
   
   return key_table
+
 # device_keys_table = get_device_keys_table(svce_loc_id_list=['86',])
 
 #%%
-def get_event_keys_table(csv_path=event_key_table_path, host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database):
+# collect all event_key(s) from server and referance [csv_path], and return a distincted event_keys_table.
+# (optional) set [use_archive]=False to always collect data from server insteed of csv
+# event_key : [tag_name] + [param_value]
+def get_event_keys_table(csv_path=event_key_table_path, host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database, use_archive=True):
   print('\ncollecting event key table ...')
   key_table = [] # the event key table
   save_path = collected_data_folder + 'event_keys_table.csv'
-  if path.exists(save_path):
+  if path.exists(save_path) and use_archive:
     with open(save_path, newline='') as csvfile:
       rows = csv.reader(csvfile)
       for row in rows:
@@ -127,9 +145,11 @@ def get_event_keys_table(csv_path=event_key_table_path, host=mssql_host, user=ms
   print('event key table archived.')
   
   return key_table
+
 # event_keys_table = get_event_keys_table(event_key_table_path, mssql_host, mssql_user, mssql_password, mssql_database)
 
 #%%
+# in progress ...
 def get_malfunction_keys_table(host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database):
   print('\ncollecting malfunction key table ...')
   
@@ -173,7 +193,20 @@ def get_malfunction_keys_table(host=mssql_host, user=mssql_user, passward=mssql_
 # malfunciont_keys_table_tmp = get_malfunction_keys_table()
 
 #%%
-def get_X_of_the_date(date: date, device_keys_table, event_keys_table, host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database):
+# get X of the date:
+# collect all needed features for predict y(s) of the specific [date] and specific device(s) in [device_keys_table] according to specific [time_window], [time_steps]
+# features:
+# assume event_keys_table = ['A', 'B', 'C'], time_steps=2
+# features of a device = [[device_key], 
+#                         [date],
+#                         [count(A) between 1st time window], 
+#                         [count(A) between 2st time window], 
+#                         [count(B) between 1st time window], 
+#                         [count(B) between 2st time window], 
+#                         [count(C) between 1st time window], 
+#                         [count(C) between 2st time window]]
+def get_X_of_the_date(date: date, device_keys_table, event_keys_table, time_window=7, time_steps=4, host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database):
+  
   conn = pymssql.connect(
     host=host,
     user=user,
@@ -185,42 +218,56 @@ def get_X_of_the_date(date: date, device_keys_table, event_keys_table, host=mssq
   value_counts = 0
   
   for device_key in device_keys_table:
-    # x = {'device_key':device_key}
-    x = {}
+    x = {'device_key':device_key, 'date':str(date)}
     for event_key in event_keys_table:
-      for i in range(1, 5):
+      for i in range(1, time_steps+1):
         x[event_key + "_" + str(i)] = 0
         value_counts += 1
     X[device_key] = x
-    
+  
   error_counts = 0
   query_counts = 0
   
+  cmd_key_constrain = ''
+  if len(device_keys_table) <= 15:
+    cmd_key_constrain = ' and (concat(RIGHT(\'000\' + CONVERT(VARCHAR(3),[svce_loc_id]), 3), dev_name) = \'' + device_keys_table[0] + '\''
+    for key in device_keys_table[1:]:
+      cmd_key_constrain += ' or concat(RIGHT(\'000\' + CONVERT(VARCHAR(3),[svce_loc_id]), 3), dev_name) = \'' + key + '\''
+    cmd_key_constrain += ')'
+  
   cmd = ""
-  for i in range(1, 5):
+  for i in range(1, time_steps + 1):
     cmd += """
-    select svce_loc_id, dev_name, concat(tag_name, param_value) as event_key, \'{0}\' as week, count(concat(tag_name, param_value)) as counts
+    select RIGHT('000' + CONVERT(VARCHAR(3),[svce_loc_id]), 3) as svce_loc_id, dev_name, concat(tag_name, param_value) as event_key, \'{0}\' as week, count(concat(tag_name, param_value)) as counts
     from [ATIM_event_alarm]
-    where create_datetime between \'{1}\' and \'{2}\' and dev_name not like \'%VATIM%\'
+    where dev_name not like \'%VATIM%\' and create_datetime between \'{1}\' and \'{2}\'{3}
     group by concat(tag_name, param_value), svce_loc_id, dev_name
-    """.format(i, str(date - timedelta(weeks=i)), str(date - timedelta(weeks=i - 1)))
-    if i < 4:
-      cmd += " union "
+    """.format(i, str(date - timedelta(days=i * time_window)), str(date + timedelta(days=1) - timedelta(days=(i - 1) * time_window)), cmd_key_constrain)  
+    if i < time_steps: cmd += " union "
+  
+  # print(cmd)
   cursor.execute(cmd)
+  
   for row in cursor:
-    query_counts += 1
-    device_key = str(row['svce_loc_id']).zfill(3) + row['dev_name']
-    if device_key in X and (row['event_key'] + "_" + str(i)) in X[device_key]:
-      X[device_key][row['event_key'] + "_" + row['week']] = int(row['counts'])
-    else: error_counts += 1
-      
-  if error_counts > 0 : print(str(error_counts) + ' errors among ' + str(query_counts) + ' query rows, ' + str(value_counts) + ' values.')
+    # query_counts += 1
+    device_key = str(row['svce_loc_id']) + row['dev_name']
+    event_key = row['event_key'] + "_" + row['week']
+    if device_key in X:
+      X[device_key][event_key] = int(row['counts'])
+    # else: error_counts += 1
+  
+  # if error_counts >= 0 : print(str(error_counts) + ' errors among ' + str(query_counts) + ' query rows, ' + str(value_counts) + ' values.')
   cursor.close()
   conn.close()
   return X.values()
 
+# X = list(get_X_of_the_date(date(2021, 2, 1), device_keys_table[:1], event_keys_table))
+
 #%%
-def get_Y_of_the_date(date: date, device_keys_table, host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database):
+# get Y of the date:
+# collect y of the specific [date] and specific device(s) in [device_keys_table] according to specific [time_window]
+# y of a device = [whether the device will malfunction in the future between time window]
+def get_Y_of_the_date(date: date, device_keys_table, time_window=7, host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database):
   conn = pymssql.connect(
     host=host,
     user=user,
@@ -239,55 +286,94 @@ def get_Y_of_the_date(date: date, device_keys_table, host=mssql_host, user=mssql
   key_not_found_counts = 0
   query_counts = 0
   
+  cmd_key_constrain = ''
+  if len(device_keys_table) <= 15:
+    cmd_key_constrain = ' and (concat(svce_loc_id, dev_name) = \'' + device_keys_table[0] + '\''
+    for key in device_keys_table[1:]:
+      cmd_key_constrain += ' or concat(svce_loc_id, dev_name) = \'' + key + '\''
+    cmd_key_constrain += ')'
+    
+  cmd = ''
+  # if len(device_keys_table) == 1:
+  #   cmd = """
+  #   select concat(svce_loc_id, dev_name) as device_key, count(dev_name) as counts
+  #   from ATIM工單_view
+  #   where dev_name not like \'%VATIM%\' and 報修日期 between \'{0}\' and \'{1}\' and concat(svce_loc_id, dev_name) = \'{2}\'
+  #   group by concat(svce_loc_id, dev_name)
+  #   """.format(str(date + timedelta(days=1)), str(date + timedelta(days=time_window + 1)), device_keys_table[0])
+  # else:
   cmd = """
   select concat(svce_loc_id, dev_name) as device_key, count(dev_name) as counts
   from ATIM工單_view
-  where dev_name not like \'%VATIM%\' and 報修日期 between \'{0}\' and \'{1}\'
+  where dev_name not like \'%VATIM%\' and 報修日期 between \'{0}\' and \'{1}\'{2}
   group by concat(svce_loc_id, dev_name)
-  """.format(str(date + timedelta(days=1)), str(date + timedelta(days=8)))
+  """.format(str(date + timedelta(days=1)), str(date + timedelta(days=time_window + 1)), cmd_key_constrain)
   cursor.execute(cmd)  
   for row in cursor:
-    query_counts += 1
+    # query_counts += 1
     if row['device_key'] in Y:
       Y[row['device_key']]['target'] = int(row['counts'])
-    else: key_not_found_counts += 1
+    # else: key_not_found_counts += 1
 
-  if key_not_found_counts > 0 : print(str(key_not_found_counts) + ' key not founds among ' + str(query_counts) + ' query rows, ' + str(value_counts) + ' values.')
+  # if key_not_found_counts > 0 : print(str(key_not_found_counts) + ' key not founds among ' + str(query_counts) + ' query rows, ' + str(value_counts) + ' values.')
   cursor.close()
   conn.close()
   return Y.values()
 
 #%%
-def get_Y_class_of_the_date():
-  pass
-
-#%%
-def get_XY_between_date(from_date: date, to_date: date, device_keys_table, event_keys_table, host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database):
+def get_XY_between_date(from_date: date, to_date: date, device_keys_table, event_keys_table, time_window_x=7, time_steps=4, time_window_y=7, host=mssql_host, user=mssql_user, passward=mssql_password, database=mssql_database, use_archive=True):
   print('\ncollecting X, Y ...')
   X = []
   Y = []
   save_path = collected_data_folder + 'XY.csv'
-  if path.exists(save_path):
+  if path.exists(save_path) and use_archive:
     print('found csv archive, collecting from csv ...')
     with open(save_path, newline='') as csvfile:
       rows = csv.reader(csvfile)
+      header = next(rows)
       for row in rows:
-        x = []
-        for value in row[:-1]:
-          x.append(int(value))
+        x = {}
+        if not row[0] in device_keys_table: continue
+        if from_date <= datetime.datetime.strptime(row[1], "%Y-%m-%d").date() < to_date:
+          for i in range(len(header)):
+            if i < 2:
+              x[header[i]] = row[i]
+            else :
+              x[header[i]] = int(row[i])
+          # x.append(row[0])
+          # x.append(row[1])
+        else : continue
         X.append(x)
         y = [int(row[-1])]
         Y.append(y)
     print('X, Y loaded from csv archive.')
-    return X, Y
+    return DataFrame(X), DataFrame(Y)
   
   print('csv archive not found, collecting from server ...')
   cursor_date = from_date
   total_dataset_size = (to_date - from_date).days
   collected_dataset_size = 0
   while cursor_date < to_date:
-    X_new = get_X_of_the_date(cursor_date, device_keys_table, event_keys_table)
-    Y_new = get_Y_of_the_date(cursor_date, device_keys_table)
+    try:
+      X_new = get_X_of_the_date(cursor_date, 
+                                device_keys_table, 
+                                event_keys_table, 
+                                time_window=time_window_x, 
+                                time_steps=time_steps,
+                                host=host, 
+                                user=user, 
+                                passward=passward,
+                                database=database)
+      Y_new = get_Y_of_the_date(cursor_date, 
+                                device_keys_table, 
+                                time_window=time_window_y,
+                                host=host, 
+                                user=user, 
+                                passward=passward,
+                                database=database)
+    except:
+      print('\nDB-Lib error message 20047, DBPROCESS is dead or not enabled, resending query ...\n')
+      continue
     X.extend(X_new)
     Y.extend(Y_new)
     collected_dataset_size += 1
@@ -297,6 +383,7 @@ def get_XY_between_date(from_date: date, to_date: date, device_keys_table, event
   
   with open(save_path, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
+    writer.writerow(list(X[0].keys()))
     for i in range(len(X)):
       # row = list(X[i].values()).extend(list(Y[i].values()))
       # print(row)
@@ -307,13 +394,6 @@ def get_XY_between_date(from_date: date, to_date: date, device_keys_table, event
       row.extend(y)
       writer.writerow(row)
   print('X, Y archived.')
-  return X, Y
-
-# %%
-def get_quick_XY():
-  device_keys_table = get_device_keys_table()
-  event_keys_table = get_event_keys_table()
-  X, Y = get_XY_between_date(date(2021, 2, 1), date(2021, 2, 15), device_keys_table, event_keys_table)
-  return X, Y
+  return DataFrame(X), DataFrame(Y)
 
 # %%
--- train.py
+++ train.py
@@ -1,111 +1,97 @@
 #%%
+from matplotlib import use
 import get_dataset
 from datetime import date
 import numpy as np
 from pandas import DataFrame
 
-device_keys_table = get_dataset.get_device_keys_table()
-event_keys_table = get_dataset.get_event_keys_table()
-X, Y = get_dataset.get_XY_between_date(date(2021, 1, 29), date(2021, 2, 23), device_keys_table, event_keys_table)
+device_keys_table = get_dataset.get_device_keys_table(use_archive=True)
+event_keys_table = get_dataset.get_event_keys_table(use_archive=True)
+
+time_steps = 2
+time_window_x = 7
+time_window_y = 7
+X, Y = get_dataset.get_XY_between_date(date(2021, 1, 29), 
+                                       date(2021, 2, 1), #date(2021, 4, 19), 
+                                       device_keys_table, 
+                                       event_keys_table,
+                                       time_window_x=time_window_x, 
+                                       time_steps=time_steps, 
+                                       time_window_y=time_window_y,
+                                       use_archive=True)
+
+X, Y = DataFrame(X), DataFrame(Y)
+
+# X = np.asarray(X).astype('float32')
+# X = np.array(X.reshape((len(X), time_steps, 352, 1)))
+
+# Y = np.asarray(Y).astype('float32')
+# Y = np.where(Y > 0, 1, 0)
+
+# print(X.shape)
+# print(Y.shape)
+
+
+# %%
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Conv2D, Flatten
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from model_bench import bench_model, test_model, plot_result
+
+num_of_features = len(X[0])
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, shuffle=True)
+
+num_of_fit = 1
+num_of_epochs = 100
+nodes_of_each_layer = [
+      4 * num_of_features, 
+      4 * num_of_features,
+]
+model_name = 'model_Conv2D_x4_1'
+
+# model = Sequential(name=model_name)
+# model.add(Dense(num_of_features * 4, activation='relu', input_shape=(num_of_features,)))
+# model.add(Dense(num_of_features * 4, activation='relu', input_shape=(num_of_features,)))
+# model.add(Dense(1, activation='sigmoid'))
+# opt = Adam(lr=1e-4, decay=1e-4)
+# model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+# # mean_absolute_error
+# # binary_crossentropy
+# print(model.summary())
+
+model = Sequential(name=model_name)
+model.add(Conv2D(filters=5, kernel_size=(time_steps, 1), activation='relu', input_shape=(time_steps, 352, 1), padding='valid'))
+# model.add(Conv2D(filters=256, kernel_size=(1, 352), padding='valid', activation='relu'))
+model.add(Flatten())
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+opt = Adam(lr=1e-4/2, decay=1e-4/2)
+model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+# mean_absolute_error
+# binary_crossentropy
+
+print(model.summary())
+
+bench_result = bench_model(model, num_of_epochs, X_train, Y_train, X_test, Y_test, model_name)
+
+plot_result(bench_result, model_name)
+
+#%%
+test_model('bench_model_backup/' + model_name, X_test, Y_test)
+
+# %%
+
+#%%
+X, Y = get_dataset.get_XY_between_date(date(2021, 1, 29), date(2021, 4, 19), device_keys_table, event_keys_table, time_window_x=7, time_steps=time_steps, time_window_y=7)
 
 X, Y = DataFrame(X), DataFrame(Y)
 
 X = np.asarray(X).astype('float32')
+X = np.array(X.reshape((len(X), time_steps, 352, 1)))
+
 Y = np.asarray(Y).astype('float32')
 Y = np.where(Y > 0, 1, 0)
 
 print(X.shape)
 print(Y.shape)
-
-# %%
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.models import load_model
-
-num_of_features = len(X[0])
-
-def generate_model():
-      model = Sequential()
-      model.add(Dense(num_of_features, activation='relu', input_shape=(num_of_features,)))
-      model.add(Dense(num_of_features * 2, activation='relu'))
-      model.add(Dense(num_of_features, activation='relu'))
-      model.add(Dense(1, activation='sigmoid'))
-
-      opt = Adam(lr=1e-4, decay=1e-6 / 200)
-      model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
-      # mean_absolute_error
-      # binary_crossentropy
-
-      print(model.summary())
-      
-      return model
-
-
-#%%
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-def bench_model(num_of_fit:int, epochs:int, X_train=X_train, Y_train=Y_train):
-      loss_list = []
-      val_loss_list = []
-      accuracy_list = []
-      val_accuracy_list = []
-      for i in range(epochs):
-            loss_list.append(0)
-            val_loss_list.append(0)
-            accuracy_list.append(0)
-            val_accuracy_list.append(0)
-      
-      for i in range(num_of_fit):
-            tmp_model = generate_model()
-            history = tmp_model.fit(X_train, Y_train, epochs=epochs, validation_split=0.2, shuffle=True)
-            tmp_model.save('trained_model')
-            for i in range(epochs):
-                  loss_list[i] += history.history['loss'][i]
-                  val_loss_list[i] += history.history['val_loss'][i]
-                  accuracy_list[i] += history.history['accuracy'][i]
-                  val_accuracy_list[i] += history.history['val_accuracy'][i]
-      
-      for i in range(epochs):
-            loss_list[i] /= num_of_fit
-            val_loss_list[i] /= num_of_fit
-            accuracy_list[i] /= num_of_fit
-            val_accuracy_list[i] /= num_of_fit
-      return loss_list, val_loss_list, accuracy_list, val_accuracy_list
-
-num_of_fit = 1
-num_of_epochs = 15
-loss_list, val_loss_list, accuracy_list, val_accuracy_list = bench_model(num_of_fit, num_of_epochs)
-
-from matplotlib import pyplot as plt
-
-plt.plot(loss_list)
-plt.plot(val_loss_list)
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.show()
-
-plt.plot(accuracy_list)
-plt.plot(val_accuracy_list)
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.show()
-
-#%%
-def test_model(X = X_test[:50]):
-      model = load_model('trained_model')
-      sample = X
-      predictions = model.predict(sample)
-      avg_diff = 0
-      for i in range(0, len(sample)):
-            pred = round(predictions[i][0], 2)
-            y = Y_test[i][0]
-            diff = abs(y - pred)
-            avg_diff += diff
-            print(str(i) + 'th' + '\tpred:' + str(pred) + '\tY:' + str(y) + '\tdiff:' + str(diff))
-      print(round(avg_diff / len(sample), 2))
-# %%
